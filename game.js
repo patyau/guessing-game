f15fba9a4ad2baf18e30cb5e11fa835e6296ea4f
Pat Yau <patrickyau@Pat-Yaus-MacBook-Pro.local>
2015-09-30 14:07:41 -0700

    add game and index files

---
+++ game.js
@@ -0,0 +1,14 @@
+var question1 = prompt("Let's play a game. I'll ask you three yes/no questions. You will try to guess the right answers... 1. Is Tulsa the capitol of Oklahoma? YES or NO");
+console.log('question1 response: ' + question1);
+alert('Your answer to question1 was ' + question1);
+
+var question2 = prompt('2. Is the song I Gotta Feeling by the Black Eyed Peas the highest selling digital single of all time? YES or NO');
+console.log('question2 response: ' + question2);
+alert('Your answer to question2 was ' + question2);
+
+var question3 = prompt('3. Did Russell Westbrook lead the NBA in scoring average per game in 2014-2015? YES or NO');
+console.log('question3 response: ' + question3);
+alert('Your answer to question3 was ' + question3);
+
+
+
