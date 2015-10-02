78018477e5fc4f40e07216daf864d0e8a6569bd6
jessethach <jessethach@gmail.com>
2015-10-02 15:11:56 -0700

    Added images to each correct response

--- game.js
+++ game.js
@@ -6,6 +6,9 @@ var one = document.getElementById('one');
 var two = document.getElementById('two');
 var three = document.getElementById('three');
 var correct = document.getElementById('correct');
+var pic1 = document.getElementById('pic1');
+var pic2 = document.getElementById('pic2');
+var pic3 = document.getElementById('pic3');
 
 var questions = ['1. Is Tulsa the capitol of Oklahoma? YES or NO', '2. Is the song "I Gotta Feeling" by the Black Eyed Peas the highest selling digital single of all time? YES or NO', '3. Did Russell Westbrook lead the league in scoring average per game in the 2014-2015 NBA season? YES or NO'];
 
@@ -17,6 +20,7 @@ function ques1() {
 	console.log('question1 toLowerCase() response: ' + question1.toLowerCase());
 	if (question1.toLowerCase() === answers[2] || question1.toLowerCase() === answers[3]) {
 		one.innerHTML = 'Good job ' + userName + '. You got the first question correct. The capital city of Oklahoma is Oklahoma City';
+		pic1.innerHTML = '<img width="200" src="img/cool-dog.jpg" />';
 		answersCorrect ++;
 	} else {
 		one.innerHTML = 'Sorry ' + userName + '. You got the first question wrong. The capital city of Oklahoma is Oklahoma City';
@@ -29,6 +33,7 @@ function ques2() {
 	console.log('question2 toLowerCase() response: ' + question2.toLowerCase());
 	if (question2.toLowerCase() === answers[2] || question2.toLowerCase() === answers[3]) {
 		two.innerHTML = 'Good job ' + userName + '. "You got the second question right". The highest selling digital single of all time is "Call Me Maybe" by Carly Rae Jespen.';
+		pic2.innerHTML = '<img width="200" src="img/cool-dog.jpg" />';
 		answersCorrect ++;
 	} else {
 		two.innerHTML = 'Sorry ' + userName + '. You got the second question wrong. The highest selling digital single of all time is "Call Me Maybe" by Carly Rae Jespen.';
@@ -41,6 +46,7 @@ function ques3() {
 	console.log('question3 toLowerCase response: ' + question3.toLowerCase());
 	if (question3.toLowerCase() === answers[0] || question3.toLowerCase() === answers[1]) {
 		three.innerHTML = 'Good job ' + userName + '. You got the third question right. Russell Westbrook averaged 28.1 ppg to lead the NBA in scoring last year.';
+		pic3.innerHTML = '<img width="200" src="img/cool-dog.jpg" />';
 		answersCorrect ++;
 	} else {
 		three.innerHTML = 'Sorry ' + userName + '. You got the third question wrong. Russell Westbrook averaged 28.1 ppg to lead the NBA in scoring last year.';
@@ -48,8 +54,8 @@ function ques3() {
 }
 
 ques1();
-ques2();
-ques3();
+window.setTimeout(ques2, 1000);
+window.setTimeout(ques3, 1000);
 
 console.log('answersCorrect = ' + answersCorrect);
 correct.innerHTML = 'Thanks for playing ' + userName + '! You got ' + answersCorrect + '/3 questions correct.';
